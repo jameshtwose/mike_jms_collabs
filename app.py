481fc4cdfcec9f1ae288477badd2499f3f452faf
James <james@Jamess-MacBook-Air.local>
2023-03-12 11:18:30 +0000

    removing the streamlit header and footer

--- app.py
+++ app.py
@@ -203,4 +203,4 @@ if check_password():
                             header {visibility: hidden;}
                             </style>
                             """
-    # st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
